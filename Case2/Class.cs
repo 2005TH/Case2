3cabac55228eaa3c98ad02d6933af30332c71f69
Thomas <Thomas@DESKTOP-EQSJMOL>
2023-04-28 12:33:56 +0200

    Add project files.

---
+++ Case2/Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Case2
+{
+    static class Hmm
+    {
+
+        public static string[,] lærerarray = new string[1,4]
+         {
+            {"Niels Olesen", "Flemming Sørensen", "Peter Lindenskov", "Henrik Poulsen"}
+         };
+        public static string[,] elevarray = new string[4,4]
+         {
+            {"Alexander Thamdru", "Allan Gawron", "Andreas Balle", "Darab Haqnazar"},
+            {"Felix Beger", "Jamie D", "Jeppe Pedersen", "Joseph Hale"},
+            {"Kamil Kulpa", "Loke Bendtsen", "Mark Larsen", "Niklas Jensen"},
+            {"Rasmus Hjorth", "Sammy Damiri", "Thomas Holmberg", "Tobias Besser"}
+         };
+        public static string[,] fagarray = new string[1,7]
+         {
+            {"Studieteknik", "Grundlæggende Programmering", "Databseprogram", "OOP", "Computerteknologi", "Clientsideprogrammering", "Netværk"}
+         };
+
+        
+    }
+}
---
+++ Case2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Case2;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        bool løkke;
+        løkke = false;
+
+        while (løkke == false)
+        {
+            
+            Console.Clear();
+            Console.WriteLine("Vælg en af fælgende valgmuligheder.");
+            Console.WriteLine("Hvis du ikke indtaster nogle at valgmulighederne, vil du starte forfra.\n");
+            Console.WriteLine("Lærer (1)");
+            Console.WriteLine("Elev  (2)");
+            Console.WriteLine("Fag   (3)");
+            Console.WriteLine("Exit  (4)");
+            Console.Write("Indtast: ");
+            string Funktion = Console.ReadLine();
+
+            if (Funktion == "1")
+            {
+                Console.WriteLine("Funktion 1");
+                Console.ReadKey();
+            }
+            if (Funktion == "2")
+            {
+                Console.WriteLine("Funktion 2");
+                Console.ReadKey();
+            }
+            if (Funktion == "3")
+            {
+                Console.WriteLine("Funktion 3");
+                Console.ReadKey();
+            }
+            if (Funktion == "4")
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
