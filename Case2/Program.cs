46118529fcfef01759095dec0961390f9b4a3f20
Thomas Holmberg <thomasholmberg05@gmail.com>
2023-04-30 22:40:51 +0200

    Klargjort til hjælp af Niels mandag

--- Case2/Program.cs
+++ Case2/Program.cs
@@ -29,18 +29,57 @@ class Program
 
             if (Funktion == "1")
             {
-                Console.WriteLine("Funktion 1");
-                Console.ReadKey();
+                Console.Clear();
+                Console.Write("Skriv det fulde navn på den lærer du vil søge på: ");
+                string lærenavn = Console.ReadLine().ToLower();
+                if (lærenavn == "lærenavn")
+                {
+                    Console.WriteLine("\nLæren underviser i Dissefag");
+                    Console.WriteLine("Fag");
+                    Console.WriteLine("Elever som har faget");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Der er ikke en lære med dette navn");
+                    Console.ReadKey();
+                }
             }
             if (Funktion == "2")
             {
-                Console.WriteLine("Funktion 2");
-                Console.ReadKey();
+                Console.Clear();
+                Console.Write("Skriv det fulde navn på den elev du vil søge på: ");
+                string elevnavn = Console.ReadLine().ToLower();
+                if (elevnavn == "elevnavn")
+                {
+                    Console.WriteLine("\nEleven har disse fag");
+                    Console.WriteLine("Fag");
+                    Console.WriteLine("Denne lærer underviser faget");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Der er ikke en elev med dette navn");
+                    Console.ReadKey();
+                }
             }
             if (Funktion == "3")
             {
-                Console.WriteLine("Funktion 3");
-                Console.ReadKey();
+                Console.Clear();
+                Console.Write("Skriv det fag du vil søge på: ");
+                string fagnavn = Console.ReadLine().ToLower();
+                if (fagnavn == "fagnavn")
+                {
+                    Console.WriteLine("\nDette fag bliver undervist af");
+                    Console.WriteLine("Læren");
+                    Console.WriteLine("Disse elever har faget");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Det er ikke et fag der bliver undervist i");
+                    Console.ReadKey();
+                }
             }
             if (Funktion == "4")
             {
