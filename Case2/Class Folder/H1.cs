d1b28b530ad5729b000685a579616066ad432d32
Thomas Holmberg <thomasholmberg05@gmail.com>
2023-05-01 15:04:24 +0200

    Final Commit

---
+++ Case2/Class Folder/H1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Øvelse;
+
+namespace Case2;
+
+interface H
+{
+    internal class H1
+    {
+        public PersonModel[] StudieTeknikElever { get; set; }
+
+        public PersonModel[] GrundlæggendeProgrammeringElever { get; set; }
+
+        public PersonModel[] DatabaseProgrammeringElever { get; set; }
+
+        public PersonModel[] OOPElever { get; set; }
+
+        public PersonModel[] ComputerteknologiElever { get; set; }
+
+        public PersonModel[] ClientsideprogrammeringElever { get; set; }
+
+        public PersonModel[] NetværkElever { get; set; }
+
+        public FagModel[] Fag { get; set; }
+
+        public PersonModel[][] AlleElever { get; set; }
+
+        public H1()
+        {
+            StudieTeknikElever = new PersonModel[]
+            {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+            };
+
+            GrundlæggendeProgrammeringElever = new PersonModel[]
+            {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+            };
+
+            DatabaseProgrammeringElever = new PersonModel[]
+    {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+    };
+
+            OOPElever = new PersonModel[]
+            {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+            };
+
+            ComputerteknologiElever = new PersonModel[]
+            {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+            };
+
+            ClientsideprogrammeringElever = new PersonModel[]
+            {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+            };
+
+            NetværkElever = new PersonModel[]
+            {
+            new PersonModel() { Fornavn = "Alexander", Efternavn = "Thamdru"},
+            new PersonModel() { Fornavn = "Allan", Efternavn = "Gawron"},
+            new PersonModel() { Fornavn = "Andreas", Efternavn = "Balle"},
+            new PersonModel() { Fornavn = "Darab", Efternavn = "Haqnazar"},
+            new PersonModel() { Fornavn = "Felix", Efternavn = "Beger"},
+            new PersonModel() { Fornavn = "Jamie", Efternavn = "D"},
+            new PersonModel() { Fornavn = "Jeppe", Efternavn = "Pedersen"},
+            new PersonModel() { Fornavn = "Joseph", Efternavn = "Hale"},
+            new PersonModel() { Fornavn = "Kamil", Efternavn = "Kulpa"},
+            new PersonModel() { Fornavn = "Loke", Efternavn = "Bendtsen"},
+            new PersonModel() { Fornavn = "Mark", Efternavn = "Larsen"},
+            new PersonModel() { Fornavn = "Niklas", Efternavn = "Jensen"},
+            new PersonModel() { Fornavn = "Rasmus", Efternavn = "Hjorth"},
+            new PersonModel() { Fornavn = "Sammy", Efternavn = "Damiri"},
+            new PersonModel() { Fornavn = "Thomas", Efternavn = "Holmberg"},
+            new PersonModel() { Fornavn = "Tobias", Efternavn = "Besser"}
+            };
+
+            Fag = new FagModel[]
+            {
+            new FagModel() { FagNavn = "Studieteknik", Lærer = new PersonModel(){ Fornavn = "Niels", Efternavn = "Olesen"} },
+            new FagModel() { FagNavn = "Grundlæggende Programmering", Lærer = new PersonModel(){ Fornavn = "Flemming", Efternavn = "Sørensen"} },
+            new FagModel() { FagNavn = "OOP", Lærer = new PersonModel() { Fornavn = "Flemming", Efternavn = "Sørensen"} },
+            new FagModel() { FagNavn = "Computerteknologi", Lærer = new PersonModel(){ Fornavn = "Niels", Efternavn = "Olesen"} },
+            new FagModel() { FagNavn = "Clientsideprogrammering", Lærer = new PersonModel(){ Fornavn = "Peter", Efternavn = "Lindenskov"} },
+            new FagModel() { FagNavn = "Netværk", Lærer = new PersonModel(){ Fornavn = "Henrik", Efternavn = "Poulsen"} }
+            };
+
+            AlleElever = new PersonModel[][]
+            {
+            StudieTeknikElever,
+            GrundlæggendeProgrammeringElever,
+            DatabaseProgrammeringElever,
+            OOPElever,
+            ComputerteknologiElever,
+            ClientsideprogrammeringElever,
+            NetværkElever
+
+            };
+        }
+    }
+}
--- Case2/Program.cs
+++ Case2/Program.cs
@@ -5,24 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using Case2;
+using Øvelse;
+using System.Net.NetworkInformation;
+using static System.Net.Mime.MediaTypeNames;
+using static Case2.H;
 
+enum Valg
+{
+    Lærer,
+    Elev,
+    Fag
+}
 class Program
 {
-
     static void Main(string[] args)
     {
         bool løkke;
         løkke = false;
 
+        H1 h1 = new H1();
+        FagModel[] fag = h1.Fag;
+        PersonModel[][] alleelever = h1.AlleElever;
+
         while (løkke == false)
         {
-            
             Console.Clear();
             Console.WriteLine("Vælg en af fælgende valgmuligheder.");
             Console.WriteLine("Hvis du ikke indtaster nogle at valgmulighederne, vil du starte forfra.\n");
-            Console.WriteLine("Lærer (1)");
-            Console.WriteLine("Elev  (2)");
-            Console.WriteLine("Fag   (3)");
+            Console.WriteLine($"" + Valg.Lærer +" (1)");
+            Console.WriteLine($"" + Valg.Elev + "  (2)");
+            Console.WriteLine($"" + Valg.Fag + "   (3)");
             Console.WriteLine("Exit  (4)");
             Console.Write("Indtast: ");
             string Funktion = Console.ReadLine();
@@ -30,56 +42,163 @@ class Program
             if (Funktion == "1")
             {
                 Console.Clear();
+                Console.WriteLine("Hvis Læreren ikke findes vil du blive smidt tilbage til menuen.");
+                Console.WriteLine("\nDer skal være stort bogstav efter hvert mellemrum.");
                 Console.Write("Skriv det fulde navn på den lærer du vil søge på: ");
-                string lærenavn = Console.ReadLine().ToLower();
-                if (lærenavn == "lærenavn")
+                string lærenavn = Console.ReadLine();
+                foreach (FagModel Fag in fag)
                 {
-                    Console.WriteLine("\nLæren underviser i Dissefag");
-                    Console.WriteLine("Fag");
-                    Console.WriteLine("Elever som har faget");
-                    Console.ReadKey();
+                    if (Fag.Lærer.Fornavn + " " + Fag.Lærer.Efternavn == lærenavn)
+                    {
+                        Console.WriteLine($"\nFag: {Fag.FagNavn}");
+
+                        PersonModel[]? elever = null;
+                        if (Fag.FagNavn == "Studieteknik")
+                        {
+                            elever = h1.StudieTeknikElever;
+                        }
+                        if (Fag.FagNavn == "Grundlæggende programmering")
+                        {
+                            elever = h1.GrundlæggendeProgrammeringElever;
+                        }
+                        if (Fag.FagNavn == "OOP")
+                        {
+                            elever = h1.OOPElever;
+                        }
+                        if (Fag.FagNavn == "Computerteknologi")
+                        {
+                            elever = h1.ComputerteknologiElever;
+                        }
+                        if (Fag.FagNavn == "Clientsideprogrammering")
+                        {
+                            elever = h1.ClientsideprogrammeringElever;
+                        }
+                        if (Fag.FagNavn == "Netværk")
+                        {
+                            elever = h1.NetværkElever;
+                        }
+                        if (elever != null)
+                        {
+                            foreach (PersonModel elev in elever)
+                            {
+                                Console.WriteLine($"Elev: {elev.Fornavn} {elev.Efternavn}");
+                            }
+                        }
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("Der er ikke en lære med dette navn");
-                    Console.ReadKey();
-                }
+                Console.Write("\nTryk på en tast for at fortsætte");
+                Console.ReadKey();
             }
             if (Funktion == "2")
             {
                 Console.Clear();
+                Console.WriteLine("Hvis eleven ikke findes vil du blive smidt tilbage til menuen.");
+                Console.WriteLine("\nDer skal være stort bogstav efter hvert mellemrum.");
                 Console.Write("Skriv det fulde navn på den elev du vil søge på: ");
-                string elevnavn = Console.ReadLine().ToLower();
-                if (elevnavn == "elevnavn")
+                string elevnavn = Console.ReadLine();
+                int c = 0;
+                if (alleelever.Length > 0)
                 {
-                    Console.WriteLine("\nEleven har disse fag");
-                    Console.WriteLine("Fag");
-                    Console.WriteLine("Denne lærer underviser faget");
-                    Console.ReadKey();
-                }
-                else
-                {
-                    Console.WriteLine("Der er ikke en elev med dette navn");
+                    foreach (PersonModel[] item in alleelever)
+                    {
+                        var Elev = item.FirstOrDefault(x => x.Fornavn + " " + x.Efternavn == elevnavn);
+                        if (Elev != null)
+                        {
+                            FagModel Fag = null;
+                            if (c == 0)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "Studieteknik");
+                            }
+                            if (c == 1)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "Grundlæggende Programmering");
+                            }
+                            if (c == 2)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "Database programmering");
+                            }
+                            if (c == 3)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "OOP");
+                            }
+                            if (c == 4)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "Computerteknologi");
+                            }
+                            if (c == 5)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "Clientsideprogrammering");
+                            }
+                            if (c == 6)
+                            {
+                                Fag = fag.FirstOrDefault(y => y.FagNavn == "Netværk");
+                            }
+                            if (Fag != null)
+                            {
+                                Console.WriteLine($"Fag: {Fag.FagNavn}");
+                                Console.WriteLine($"Lærer: {Fag.Lærer.Fornavn} {Fag.Lærer.Efternavn}");
+                            }
+                        }
+                        c++;
+                    }
+                    Console.Write("\nTryk på en tast for at fortsætte");
                     Console.ReadKey();
                 }
             }
             if (Funktion == "3")
             {
                 Console.Clear();
+                Console.WriteLine("Hvis Faget ikke findes vil du blive smidt tilbage til menuen.");
+                Console.WriteLine("\nDer skal være stort bogstav efter hvert mellemrum.");
                 Console.Write("Skriv det fag du vil søge på: ");
-                string fagnavn = Console.ReadLine().ToLower();
-                if (fagnavn == "fagnavn")
+                string fagnavn = Console.ReadLine();
+                foreach (FagModel Fag in fag)
                 {
-                    Console.WriteLine("\nDette fag bliver undervist af");
-                    Console.WriteLine("Læren");
-                    Console.WriteLine("Disse elever har faget");
-                    Console.ReadKey();
+                    if (Fag.FagNavn == fagnavn)
+                    {
+                        Console.WriteLine($"Læren: {Fag.Lærer.Fornavn} {Fag.Lærer.Efternavn}");
+                        break;
+                    }
                 }
-                else
+                PersonModel[]? elever = null;
+                if (fagnavn == "Studieteknik")
                 {
-                    Console.WriteLine("Det er ikke et fag der bliver undervist i");
-                    Console.ReadKey();
+                    elever = h1.StudieTeknikElever;
                 }
+                if (fagnavn == "Grundlæggende Programmering")
+                {
+                    elever = h1.GrundlæggendeProgrammeringElever;
+                }
+                if (fagnavn == "OOP")
+                {
+                    elever = h1.OOPElever;
+                }
+                if (fagnavn == "Computerteknologi")
+                {
+                    elever = h1.ComputerteknologiElever;
+                }
+                if (fagnavn == "Clientsideprogrammering")
+                {
+                    elever = h1.ClientsideprogrammeringElever;
+                }
+                if (fagnavn == "Netværk")
+                {
+                    elever = h1.NetværkElever;
+                }
+                if (elever != null)
+                {
+                    foreach (PersonModel elev in elever)
+                    {
+                        Console.WriteLine($"Elev: {elev.Fornavn} {elev.Efternavn}");
+                    }
+                }
+                if (elever == null)
+                {
+                    Console.WriteLine("Det er ikke et fag vi underviser i, prøv igen");
+                }
+
+                Console.Write("\nTryk på en tast for at fortsætte");
+                Console.ReadKey();
             }
             if (Funktion == "4")
             {
